e458f9d8ad4d442ff136e3d48fff1df65ab3132a
saad-7223 <saadsyed837@gmail.com>
2022-11-09 00:08:29 +0530

    df

--- java_DS.java/Arrayutil.java
+++ java_DS.java/Arrayutil.java
@@ -1,8 +1,8 @@
 import java.util.Arrays;
 public class Arrayutil {
     public static void main(String [] args){
-        int[] arr = {6,0,5,0,0,1,3};
-        int j=0;
+        int[] arr = {2,4,1,8,6,3,7};
+        /*int j=0;
         for(int i=0;i<arr.length;i++){
             if(arr[i]!=0 && arr[j]==0){
                 int temp=arr[i];
@@ -14,7 +14,13 @@ public class Arrayutil {
             }
         }
         for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i] + " ");
+            System.out.print(arr[i] + "");
+        }*/
+        int n=arr.length+1;//Finding the missing number
+        int sum = (n*(n+1))/2;
+        for(int num:arr){
+            sum-=num;
         }
+        System.out.println(sum);
     }
 }
