2a06404b17d591b854773bad74a5104563fc1ebe
saad-7223 <saadsyed837@gmail.com>
2022-11-08 22:56:06 +0530

    a

--- java_DS.java/Arrayutil.java
+++ java_DS.java/Arrayutil.java
@@ -2,16 +2,19 @@ import java.util.Arrays;
 public class Arrayutil {
     public static void main(String [] args){
         int[] arr = {6,0,5,0,0,1,3};
-        /*for(int i =0;i<arr.length;i++){
-            if (arr[i]==0 && arr[i+1]!=0){
-                int temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = temp;
-            }
-        }*/
-        //System.out.println(arr);
+        int j=0;
         for(int i=0;i<arr.length;i++){
-            System.out.println(arr[i]);
+            if(arr[i]!=0 && arr[j]==0){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+            if(arr[j]!=0){
+                j++;
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
         }
     }
 }
