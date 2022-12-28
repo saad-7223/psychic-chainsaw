a8f95eb576b1a985dc400ff990da79c6548804d2
saad-7223 <saadsyed837@gmail.com>
2022-12-28 21:40:34 +0530

    sd

---
+++ temp.java/EvenOddApp.java
@@ -0,0 +1,38 @@
+import java.lang.Exception;
+import java.util.Scanner;
+class OddException extends Exception{ 
+    OddException(){
+        super("Odd number exception");
+    }
+    OddException(String msg){
+        super(msg);
+    }
+}
+class EvenOdd{
+    void halfOf(int num){
+    try{
+        if(num%2 != 0)
+            throw(new OddException()); // Statement 2
+        else
+            if (num == 0)
+                throw(new OddException());
+            else
+                System.out.print("\n\t" + num + " is an even number and its half is:"+ (num/2));
+    }
+    catch(OddException Ex){
+        System.out.print("\n\tError : " + Ex.getMessage());
+    }  
+    System.out.print("\n\tEnd of program");
+    }
+}
+class EvenOddApp{
+    public static void main(String[] args){
+    int num;
+    Scanner Sc = new Scanner(System.in);
+    System.out.print("\n\tEnter any number : ");
+    num = Integer.parseInt(Sc.nextLine());
+    EvenOdd EO= new EvenOdd();
+    EO.halfOf(num);
+    Sc.close();
+    }
+}
