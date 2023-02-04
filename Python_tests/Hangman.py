3e076a3960997d406bf7124d6c879cb7a2273322
saad-7223 <saadsyed837@gmail.com>
2023-02-04 12:55:30 +0530

    xdf

--- Python_tests/Hangman.py
+++ Python_tests/Hangman.py
@@ -1,22 +1,34 @@
-import H1
-print("HANGMAN GAME")
-H1.hman()
-s="beekeeper"
-count =6;e=[];p="";m=0
-while(count!=0):
-    print("Lives remaining = ",count)
-    w=input("guess a letter : ")
-    for i in range(0,len(s)):
-        if s[i] == w:
-            e.insert(i,s[i])            
-        else:
-            m+=1
+chosenWord = "beekeeper"
+gameOver = False # state variable
+lives = 6
+# create blanks
+display = []
+for _ in chosenWord:
+    display += '_'
 
-    count-=1
-    H1.mistake(m)
-for i in e:
-    p+=i 
-if(s==p):
-    print("you won")
-else:
-    print("you lose")
+
+while not gameOver:
+    guess = input("Guess a letter: ").lower()[0] #capture 1 letter
+    
+    if guess in display:
+        print(f"You already guessed {guess}, try a different letter")
+    
+    for i in range(0,len(chosenWord)):
+        if guess == chosenWord[i]:
+            display[i] = chosenWord[i]
+            
+              
+    # jump out of the if loop into the for loop and do the check there
+    if guess not in chosenWord:
+        print(f"You guessed {guess} that is not in the word, you loose a life")
+        lives -=1
+        if lives == 0:
+            gameOver = True
+            print('You Loose')
+    print(display)
+    print(hangman_art.stages[lives])
+
+    if '_' not in display:
+        gameOver = True # state variable
+        print("You Win")
+        
